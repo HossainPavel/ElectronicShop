354a16d5a0878544b8fb33f520e6331469ec0f40
HossainPavel <89180472+HossainPavel@users.noreply.github.com>
2021-08-19 12:10:25 +0600

    ABCDEF
    XYZ

---
+++ ElecShop/AddMember.Designer.cs
@@ -0,0 +1,173 @@
+﻿namespace ElecShop
+{
+    partial class AddMember
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtName = new System.Windows.Forms.TextBox();
+            this.comboType = new System.Windows.Forms.ComboBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtPass = new System.Windows.Forms.TextBox();
+            this.btnBackSignin = new System.Windows.Forms.Button();
+            this.btnPrev = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            this.button1.Location = new System.Drawing.Point(497, 314);
+            this.button1.Margin = new System.Windows.Forms.Padding(4);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(116, 52);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Add";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(192, 101);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(45, 17);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Name";
+            // 
+            // txtName
+            // 
+            this.txtName.Location = new System.Drawing.Point(353, 101);
+            this.txtName.Margin = new System.Windows.Forms.Padding(4);
+            this.txtName.Name = "txtName";
+            this.txtName.Size = new System.Drawing.Size(259, 22);
+            this.txtName.TabIndex = 2;
+            this.txtName.TextChanged += new System.EventHandler(this.txtName_TextChanged);
+            // 
+            // comboType
+            // 
+            this.comboType.FormattingEnabled = true;
+            this.comboType.Items.AddRange(new object[] {
+            "Admin",
+            "Employee",
+            "Customer"});
+            this.comboType.Location = new System.Drawing.Point(353, 236);
+            this.comboType.Margin = new System.Windows.Forms.Padding(4);
+            this.comboType.Name = "comboType";
+            this.comboType.Size = new System.Drawing.Size(259, 24);
+            this.comboType.TabIndex = 3;
+            this.comboType.SelectedIndexChanged += new System.EventHandler(this.comboType_SelectedIndexChanged);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(192, 171);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(69, 17);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Password";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(192, 246);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(74, 17);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "User Type";
+            // 
+            // txtPass
+            // 
+            this.txtPass.Location = new System.Drawing.Point(353, 171);
+            this.txtPass.Margin = new System.Windows.Forms.Padding(4);
+            this.txtPass.Name = "txtPass";
+            this.txtPass.Size = new System.Drawing.Size(259, 22);
+            this.txtPass.TabIndex = 6;
+            // 
+            // btnBackSignin
+            // 
+            this.btnBackSignin.Location = new System.Drawing.Point(667, 530);
+            this.btnBackSignin.Margin = new System.Windows.Forms.Padding(4);
+            this.btnBackSignin.Name = "btnBackSignin";
+            this.btnBackSignin.Size = new System.Drawing.Size(152, 52);
+            this.btnBackSignin.TabIndex = 7;
+            this.btnBackSignin.Text = "Sign Out";
+            this.btnBackSignin.UseVisualStyleBackColor = true;
+            this.btnBackSignin.Click += new System.EventHandler(this.btnBackSignin_Click);
+            // 
+            // btnPrev
+            // 
+            this.btnPrev.Location = new System.Drawing.Point(497, 530);
+            this.btnPrev.Margin = new System.Windows.Forms.Padding(4);
+            this.btnPrev.Name = "btnPrev";
+            this.btnPrev.Size = new System.Drawing.Size(144, 52);
+            this.btnPrev.TabIndex = 8;
+            this.btnPrev.Text = "<<Previous";
+            this.btnPrev.UseVisualStyleBackColor = true;
+            this.btnPrev.Click += new System.EventHandler(this.btnPrev_Click);
+            // 
+            // AddMember
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Salmon;
+            this.ClientSize = new System.Drawing.Size(895, 628);
+            this.Controls.Add(this.btnPrev);
+            this.Controls.Add(this.btnBackSignin);
+            this.Controls.Add(this.txtPass);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.comboType);
+            this.Controls.Add(this.txtName);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "AddMember";
+            this.Text = "AddEmployee";
+            this.Load += new System.EventHandler(this.AddMember_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtName;
+        private System.Windows.Forms.ComboBox comboType;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtPass;
+        private System.Windows.Forms.Button btnBackSignin;
+        private System.Windows.Forms.Button btnPrev;
+    }
+}
---
+++ ElecShop/AddMember.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.Common;
+using ElecShop.DataAccess;
+
+
+namespace ElecShop
+{
+    public partial class AddMember : Form
+    {
+        public AddMember()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            UserLogin ul = new UserLogin();
+            Userdataaccess ud = new Userdataaccess();
+            
+            ul.Name = txtName.Text;
+            ul.Password = txtPass.Text;
+            ul.UserType = comboType.Text;
+            int a = ud.AddUser(ul);
+
+
+            if (a == 1)
+            { 
+                MessageBox.Show("User Has Been Added");
+                txtName.Text = "";
+                txtPass.Text = "";
+                comboType.Text = "";
+            }
+
+            else 
+            { MessageBox.Show("User Has Not Been Added"); }
+
+        }
+
+        private void btnBackSignin_Click(object sender, EventArgs e)
+        {
+            LoginForm LF = new LoginForm();
+            LF.Visible = true;
+            this.Visible = false;
+        }
+
+        private void AddMember_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnPrev_Click(object sender, EventArgs e)
+        {
+            this.Visible = false;
+
+            OptionView OV = new OptionView();
+            OV.Visible = true;
+           
+        }
+
+        private void comboType_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void txtName_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
